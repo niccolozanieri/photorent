6d2f3292313e7fae64730ab402c6863396da9182
niccolo_zanieri <niccolo.zanieri.13@gmail.com>
2023-03-05 11:10:03 +0100

    Added brands enumerations

---
+++ camera_brand.h
@@ -0,0 +1,18 @@
+//
+// Created by niccolo on 05/03/23.
+//
+
+#ifndef PHOTORENT_CAMERA_BRAND_H
+#define PHOTORENT_CAMERA_BRAND_H
+
+enum CameraBrand {
+    SONY,
+    NIKON,
+    CANON,
+    FUJIFILM,
+    LEICA,
+    PENTAX,
+    PANASONIC,
+    RICOH
+};
+#endif //PHOTORENT_CAMERA_BRAND_H
---
+++ lens_brand.h
@@ -0,0 +1,23 @@
+//
+// Created by niccolo on 05/03/23.
+//
+
+#ifndef PHOTORENT_LENS_BRAND_H
+#define PHOTORENT_LENS_BRAND_H
+
+enum LensBrand {
+    SONY,
+    NIKON,
+    CANON,
+    SAMYANG,
+    LEICA,
+    SIGMA,
+    ZEISS,
+    TOKINA,
+    FUJIFILM,
+    PENTAX,
+    OLYMPUS,
+    PANASONIC
+};
+
+#endif //PHOTORENT_LENS_BRAND_H
